6f7a6d7eb7c5880cebd498057b5922ce2baa7fc5
KenboZaka <kenbobusiness@gmail.com>
2019-05-09 22:41:59 +0900

    git clarken first commit

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// 記事
 Route::get('/article', 'ArticleController@index');
 Route::get('/article/{article}', 'ArticleController@show');
-
+// 投稿
 Route::post('/post/{article}', 'PostController@create');
 Route::get('/detail/{post}', 'PostController@show');
 Route::get('/edit/{post}', 'PostController@edit');
 Route::post('/update/{post}', 'PostController@update');
 Route::post('/delete/{post}', 'PostController@delete');
-
+// 投稿へのコメント
 Route::post('/comment/{post}', 'CommentController@create');
 Route::get('/edit/{post}/{comment}', 'CommentController@edit');
 Route::post('/update/{comment}', 'CommentController@update');
